85f6168f6753c8f85e59c34fe2e7ebf73c24c6eb
sotirovski <paperfliesbetter@gmail.com>
2023-11-15 17:10:51 +0100

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -10,4 +13,12 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@Bean
+	public RedisTemplate<Long, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+		RedisTemplate<Long, Object> template = new RedisTemplate<>();
+		template.setConnectionFactory(connectionFactory);
+		template.afterPropertiesSet();
+		template.watch(1l);
+    		return template;
+	}
 }
